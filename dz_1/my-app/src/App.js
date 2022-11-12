2e6ab68d23baef7822ca1d9e09cfe174f11da3f7
Baktybeks <baktybeks@gmail.com>
2022-11-13 01:15:34 +0600

    dz_1 finish

--- dz_1/my-app/src/App.js
+++ dz_1/my-app/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 
+function AboutUs() {
+    return (
+        <>
+            <h1>AboutUs</h1>
+        </>
+    )
+}
+
+function Portfolio() {
+    return (
+        <>
+            <h1>Portfolio</h1>
+        </>
+    )
+}
+
+
 function App() {
-  return (
-    <div className="App">
-        hello world
-        hello world2
-    </div>
-  );
+    return (
+        <div className="App">
+            <AboutUs/>
+            <Portfolio/>
+        </div>
+    );
 }
 
 export default App;
